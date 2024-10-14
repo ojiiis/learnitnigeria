c7b2c50f74e606a10311c0f8e92342537d65a8d4
ojiiis <official.ojingirisamuel@gmail.com>
2024-10-14 14:05:29 +0100

    44

--- js.js
+++ js.js
@@ -1,24 +1,24 @@
-const typeAni = (elem,wait) =>{
+const typeAni = (elem,set ={}) =>{
     var content = elem.innerText;
-    //elem.innerHTML = "";
     var start = 0;
     elem.innerHTML = content[start++];
-    addCursor(elem);
+    addCursor(elem,set);
     const typing = ()=>{
+
        if(start == content.length){
-       start = 0
-       elem.innerText = "";
-       addCursor(elem);
+     clearInterval(run);
+      clearInterval(main);
+     return;
        }
        
        elem.innerText = elem.innerText + content[start++];   
-       addCursor(elem);
+       addCursor(elem,set);
     }
     
-    
+
     var run = setInterval(typing,200);
     var cursorOn = false;
-    setInterval(function(){
+    var main = setInterval(function(){
         if(cursorOn == false){
             clearInterval(run);
             cursorOn = true;
@@ -26,18 +26,18 @@ const typeAni = (elem,wait) =>{
            run = setInterval(typing,200);
            cursorOn = false;
         }
-    },wait);
+    },set.delay);
 }
 
 
 
     
- function addCursor(elem){
+ function addCursor(elem,set){
     let cursor = document.createElement("div");
     cursor.style.display = "inline-block";
     cursor.style.width = "2px";
     cursor.style.height= (elem.offsetHeight*0.52)+"px";
-    cursor.style.background = "black";
+    cursor.style.background = set.cursorColor;
     cursor.style.lineHeight = "0.01";
     
     elem.appendChild(cursor);
@@ -58,3 +58,44 @@ const showCourse =()=>{
 const hideCourse =()=>{
     document.getElementById("single-course-details").style.display ="none";
 }
+
+
+var appliedList = [
+    {
+        name:"Aliu Jamiu",
+        pic:""
+    },
+     {
+        name:"Gbadamosi Joshua",
+        pic:""
+    },
+    {
+        name:"Gabrel Musa",
+        pic:""
+    }
+];
+
+var cIndex = 0;
+setInterval(function(){
+ if(cIndex == appliedList.length){
+cIndex = 0;
+ }
+ var text = `<div id="applied-img" style="background-image:url(${appliedList[cIndex].pic})"></div>
+         <div id="applied-details">
+            <b>${appliedList[cIndex].name}</b>
+            <div>Just applied for web development course.</div>
+         </div>`;
+
+ if(document.getElementById("applied-alert")){
+document.getElementById("applied-alert").style.animation = "toRight 3s;"
+    setTimeout(function(){
+        document.getElementById("applied-alert").remove();
+    },2000);
+ }
+ let a = document.createElement('div');
+a.id = "applied-alert";
+a.innerHTML = text;
+document.body.appendChild(a);
+a.style.animation = "fromBottom 4s;";
+cIndex++;
+},4000);
